06e0e17f1663c3678c09377de5ab7831cd418336
Jason Yao <Hello@JasonYao.com>
2023-10-26 09:42:20 +0000

    [Build] dc759b3377553be5860300e240fb505b6a27a9cf

--- data/tests/test_bio.py
+++ data/tests/test_bio.py
@@ -1,6 +1,8 @@
 from ..resume_data import BIOGRAPHY
 
-LINKEDIN_SUMMARY_CHARACTER_LIMIT = 2000
+# See https://github.com/JasonYao/resume/issues/39#issuecomment-1780577908
+# for more information on LinkedIn character limits
+LINKEDIN_SUMMARY_CHARACTER_LIMIT = 2600
 
 
 def test_bio_conforms_to_linkedin_character_limit():
