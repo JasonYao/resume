ee4bb3743a7889e94c59636d23f53d63323fa1fd
Jason Yao <Hello@JasonYao.com>
2025-12-05 12:59:56 -0500

    Removed postal code, removed mediamath and updated current role

--- data/tests/test_bio.py
+++ data/tests/test_bio.py
@@ -6,4 +6,4 @@ LINKEDIN_SUMMARY_CHARACTER_LIMIT = 2600
 
 
 def test_bio_conforms_to_linkedin_character_limit():
-    assert len(BIOGRAPHY) < LINKEDIN_SUMMARY_CHARACTER_LIMIT
+  assert len(BIOGRAPHY) < LINKEDIN_SUMMARY_CHARACTER_LIMIT
